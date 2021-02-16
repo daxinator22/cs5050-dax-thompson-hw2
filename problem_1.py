8b5d1b9e7ab59b2b9408ed7f88c3b1be6ba46413
Dax Thompson <daxpault@gmail.com>
2021-02-16 14:51:46 -0700

    O(nlogn) recursive algorithm

--- problem_1.py
+++ problem_1.py
@@ -1,29 +1,44 @@
-A = [1, 2, 3, 4, 19]
-B = [1, 2, 3, 4, 5]
-x = 6
-counter = 0
+A = [1, 2, 3, 4, 4, 19]
+B = [1, 2, 2, 4, 4, 5]
+x = 24
 
 def find_B_sum(A, B, x):
     if len(B) == 0:
-        return
+        return False
     
     if B[0] + A[0] == x:
         print(f'Found solution at {A[0]} and {B[0]}')
-        return
+        return True
 
     else:
-        find_B_sum(A, B[1:], x)
+        midB = int(len(B) / 2)
+        if midB == 0:
+            midB = 1
+
+        if midB < len(B) and B[midB] + A[0] > x:
+            return find_B_sum(A, B[:midB], x)
+
+        else:
+            return find_B_sum(A, B[midB:], x)
 
 def find_sum(A, B, x):
 
     if len(A) == 0:
-        return
+        return False
 
 
     else:
-        find_B_sum(A, B, x)
-        find_sum(A[1:], B, x)
+        found = find_B_sum(A, B, x)
+        midA = int(len(A) / 2)
+        if midA == 0:
+            midA = 1
+
+        if midA < len(A) and A[midA] + B[0] > x:
+            find_sum(A[:midA], B, x)
+
+        elif not :
+            return find_B_sum(A[midA:], B,  x)
     
 
 
-find_sum(B, A, x)
+find_sum(A, B, x)
