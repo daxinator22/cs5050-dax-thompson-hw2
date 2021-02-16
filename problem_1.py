0c59bc99828261bc84128119858259d113f459c6
Dax Thompson <daxpault@gmail.com>
2021-02-16 13:32:55 -0700

    O(n^2) algorithm completed

--- problem_1.py
+++ problem_1.py
@@ -1,1 +1,12 @@
-print('Test')
+A = [1, 2, 3, 4, 19]
+B = [1, 2, 3, 4, 5]
+x = 20
+
+def find_sum(A, B, x):
+    for eleA in A:
+        for eleB in B:
+            if eleA + eleB == x:
+                print(f'Found solution of {eleA} and {eleB}')
+                break
+
+find_sum(A, B, x)
