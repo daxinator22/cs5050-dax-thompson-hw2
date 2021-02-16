da8634b562f6ea28d9ca98f80c39873d55ce5d53
Dax Thompson <daxpault@gmail.com>
2021-02-16 13:43:32 -0700

    O(n^2) algorithm is now recursive

--- problem_1.py
+++ problem_1.py
@@ -1,12 +1,20 @@
 A = [1, 2, 3, 4, 19]
 B = [1, 2, 3, 4, 5]
-x = 20
+x = 0
 
-def find_sum(A, B, x):
-    for eleA in A:
-        for eleB in B:
-            if eleA + eleB == x:
-                print(f'Found solution of {eleA} and {eleB}')
-                break
+def find_sum(A, B, x, startA, startB):
+    if startA >= len(A):
+        print('No solution was found')
+        return
 
-find_sum(A, B, x)
+    if A[startA] + B[startB] == x:
+        print(f'Found solution at A[{startA}] and B[{startB}]')
+        return
+
+    elif startB + 1 == len(B):
+        find_sum(A, B, x, startA + 1, 0)
+        
+    else:
+        find_sum(A, B, x, startA, startB + 1)
+
+find_sum(A, B, x, 0, 0)
