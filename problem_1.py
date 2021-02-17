80c2e48e07ce53886f82691517e18e3db384c75e
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2021-02-16 23:13:57 -0700

    Comparision O(n) if indices are the same

--- problem_1.py
+++ problem_1.py
@@ -1,54 +1,20 @@
 A = [1, 2, 3, 4, 4, 19]
 B = [1, 2, 2, 4, 4, 5]
-x = 23
+x = 24
 
-def find_B_sum(A, B, x):
-    if len(B) == 0:
-        return False
-    
-    if B[0] + A[0] == x:
-        print(f'Found solution at {A[0]} and {B[0]}')
-        return True
-
-    else:
-        midB = int(len(B) / 2)
-        if midB == 0:
-            midB = 1
-
-        if midB < len(B) and B[midB] + A[0] > x:
-            return find_B_sum(A, B[:midB], x)
-
-        else:
-            return find_B_sum(A, B[midB:], x)
-
-def find_sum(A, B, x):
-
+def compare_divide(A, B):
     if len(A) == 0:
-        return False
-
-
-    else:
-        if find_B_sum(A, B, x):
-            return True
-
-        midA = int(len(A) / 2)
-        if midA == 0:
-            midA = 1
-
-        if midA < len(A) and A[midA] + B[0] > x:
-            find_sum(A[:midA], B, x)
-
-        else:
-            return find_sum(A[midA:], B,  x)
-    
-def print_divide(A, B):
-    if len(A) == 0:
-        return ''
+        return
     
     midA = int(len(A) / 2)
     if midA == 0:
-        return f'A:{A[midA]} B:{B[midA]} '
-    else:
-        return print_divide(A[:midA], B[:midA]) + print_divide(A[midA:], B[midA:])
+        if B[0] + A[0] == x:
+            print(f'A:{A[0]} B:{B[0]}')
+        else:
+            return
 
-print(print_divide(A, B))
+    else:
+        compare_divide(A[:midA], B[:midA])
+        compare_divide(A[midA:], B[midA:])
+
+compare_divide(A, B)
